e36c7f9c8a9aaf0ad276ee74dd7ed78312b04286
MetaMaxfield <metamaxfield@gmail.com>
2025-09-08 22:22:15 +0400

    add Location and Event models

--- src/events/models.py
+++ src/events/models.py
@@ -1,1 +1,40 @@
-# from django.db import models
+import uuid
+
+from django.db import models
+
+
+class Location(models.Model):
+    """Location model."""
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField("Название", max_length=50)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Площадка"
+        verbose_name_plural = "Площадки"
+
+
+class Event(models.Model):
+    """Event model."""
+
+    OPEN_STATUS = "open"
+    CLOSED_STATUS = "closed"
+    STATUS_TYPES = [(OPEN_STATUS, "Открыт"), (CLOSED_STATUS, "Закрыт")]
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField("Название", max_length=50)
+    date = models.DateTimeField("Дата проведения мероприятия")
+    status = models.CharField("Текущий статус", choices=STATUS_TYPES)
+    location = models.ForeignKey(
+        "Location", blank=True, null=True, on_delete=models.SET_NULL
+    )
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Мероприятие"
+        verbose_name_plural = "Мероприятия"
