4338b9218a135019475945f647a22a7a57f07817
MetaMaxfield <metamaxfield@gmail.com>
2025-09-09 14:32:32 +0400

    add EventListSerializer

---
+++ src/events/serializers.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+
+from src.events.models import Event
+
+
+class EventListSerializer(serializers.ModelSerializer):
+    location = serializers.SlugRelatedField(slug_field="title", read_only=True)
+
+    class Meta:
+        model = Event
+        fields = "__all__"
