7d3577796f56f697708076136355554c6ebd9073
MetaMaxfield <metamaxfield@gmail.com>
2025-09-10 10:11:56 +0400

    add LoginSerializer

--- src/auth/serializers.py
+++ src/auth/serializers.py
@@ -1,4 +1,4 @@
-from django.contrib.auth import get_user_model
+from django.contrib.auth import authenticate, get_user_model
 from rest_framework import serializers
 
 
@@ -9,3 +9,13 @@ class RegisterSerializer(serializers.ModelSerializer):
     class Meta:
         model = get_user_model()
         fields = ("username", "password")
+
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField()
+
+    def validate(self, attrs):
+        if not (user := authenticate(**attrs)):
+            raise serializers.ValidationError({"error": "Invalid username or password"})
+        return user
