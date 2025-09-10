cc136121b70704bdfa10b02860ee6cdc20398685
MetaMaxfield <metamaxfield@gmail.com>
2025-09-10 17:29:49 +0400

    add SyncResult

--- src/sync/models.py
+++ src/sync/models.py
@@ -1,1 +1,25 @@
-# Create your models here.
+from django.core.validators import MinValueValidator
+from django.db import models
+
+
+class SyncResult(models.Model):
+    created_at = models.DateTimeField("Дата и время синхронизации", auto_now_add=True)
+    added_count = models.IntegerField(
+        "Количество новых добавленных мероприятий",
+        validators=[
+            MinValueValidator(0),
+        ],
+    )
+    updated_count = models.IntegerField(
+        "Количество обновленных мероприятий",
+        validators=[
+            MinValueValidator(0),
+        ],
+    )
+
+    def __str__(self):
+        return f"Синхронизация от {self.created_at}"
+
+    class Meta:
+        verbose_name = "Результат синхронизации"
+        verbose_name_plural = "Результаты синхронизаций"
